d4031ef0cda2582612bce4ece1e29c5322560065
pheetah <eyupfatih.ersoy@hotmail.com>
2021-11-01 21:17:44 +0300

    revised run configuration

--- run.sh
+++ run.sh
@@ -3,9 +3,14 @@ FILE=go.mod
 if [ -f "$FILE" ]; 
 then
     echo "starting the program..."
+    echo "..."
     go run github.com/eyupfatihersoy/app-tryout-1
 else
     echo "$FILE does not exist."
+    echo "configuring files..."
+    go mod init
+    echo "..."
+    go run github.com/eyupfatihersoy/app-tryout-1
 fi
 
 echo "finished executing."
